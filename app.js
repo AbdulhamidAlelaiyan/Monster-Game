8b27c0f6c1d7c7f499e6602ec7936891b0783ed4
AbdulhamidAlelaiyan <quickisslow191@gmail.com>
2020-03-12 15:19:26 +0300

    Player can now attack

--- app.js
+++ app.js
@@ -9,6 +9,41 @@ new Vue({
         startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = this.monsterHealth = 100;
+        },
+        attack: function () {
+            this.monsterHealth -= this.calculateDamage(3, 10);
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function (min, max) {
+
+        },
+        calculateDamage: function () {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 });
