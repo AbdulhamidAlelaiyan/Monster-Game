7dd9aba177e4457d717d3682d7380e0520c41ee2
AbdulhamidAlelaiyan <quickisslow191@gmail.com>
2020-03-12 20:51:27 +0300

    Player can now special attack and heal and give up

--- app.js
+++ app.js
@@ -4,27 +4,46 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
+        turns: [],
     },
     methods: {
         startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = this.monsterHealth = 100;
         },
-        attack: function () {
-            this.monsterHealth -= this.calculateDamage(3, 10);
-            this.playerHealth -= this.calculateDamage(5, 12);
+        attack: function (event, special = false) {
+            let damage;
+            if (special) {
+                damage = this.calculateDamage(6, 20);
+                this.monsterHealth -= damage;
+            } else {
+                damage = this.calculateDamage(3, 10);
+                this.monsterHealth -= damage;
+            }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage,
+            });
+            this.monsterAttacks();
             this.checkWin();
         },
-        specialAttack: function () {
-
-        },
         heal: function () {
-
+            if (this.playerHealth <= 90) this.playerHealth += 10;
+            else this.playerHealth = 100;
+            // this.monsterAttacks();
+            this.checkWin();
         },
         giveUp: function (min, max) {
-
+            this.gameIsRunning = false;
         },
-        calculateDamage: function () {
+        monsterAttacks: function (min, max) {
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Player hits Monster for ' + damage,
+            });
+        },
+        calculateDamage: function (min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
         },
         checkWin: function () {
