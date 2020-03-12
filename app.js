12a4e68e6dfc8c2ab1b94fe8fff157afbb9138ac
AbdulhamidAlelaiyan <quickisslow191@gmail.com>
2020-03-12 14:47:35 +0300

    User can start new game

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
+    },
+    methods: {
+        startGame: function () {
+            this.gameIsRunning = true;
+            this.playerHealth = this.monsterHealth = 100;
+        }
     }
 });
